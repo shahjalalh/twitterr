b1682132c2b2374985eddbeec323f696bf0cf788
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2018-10-21 06:30:41 +0600

    settings updated

--- twitterr/settings/production.py
+++ twitterr/settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!#_y18-7t1rb)l1_gc^r(rr(z0w6ke(ptmc3ltxydr9@ysh3ej'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
