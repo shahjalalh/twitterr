7d6f70f155fe32741aeb20ac79d6bc836563990b
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2018-10-21 22:20:39 +0600

    initial setup complete

--- twitterr/settings/local.py
+++ twitterr/settings/local.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'twitterr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    # will not be served, long term storage
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+# will be served
+STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
