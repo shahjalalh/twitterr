6e422a577093d2ea4baaa250c2eda89f56902a0c
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2018-10-25 07:06:20 +0600

    class based view

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.db.models import Q
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView
+)
 
 from .models import Tweet
 
 
 # Create your views here.
+
+
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
+
+
+class TweetListView(ListView):
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        query = self.request.GET.get('q', None)
+
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+            )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        # context['create_form'] = TweetModelForm()
+        context['create_url'] = reverse_lazy("tweets:create")
+
 
 def tweet_detail_view(request, pk=None):
     # obj = Tweet.objects.get(pk=pk)
